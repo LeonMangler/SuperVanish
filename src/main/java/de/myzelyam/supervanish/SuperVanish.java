f393eb5033f61180cae29c927394c1bca4436666
Leon <dev@myzelyam.de>
2018-01-31 18:47:02 +0100

    Bump version to v6.0.5

--- src/main/java/de/myzelyam/supervanish/SuperVanish.java
+++ src/main/java/de/myzelyam/supervanish/SuperVanish.java
@@ -52,8 +52,9 @@ import lombok.Getter;
 
 public class SuperVanish extends JavaPlugin implements SuperVanishPlugin {
 
-    public static final String[] NON_REQUIRED_SETTINGS_UPDATES = {"6.0.0", "6.0.1", "6.0.2", "6.0.3"},
-            NON_REQUIRED_MESSAGES_UPDATES = {"6.0.0", "6.0.1", "6.0.2", "6.0.3"};
+    public static final String[] NON_REQUIRED_SETTINGS_UPDATES = {"6.0.0", "6.0.1", "6.0.2", "6.0.3",
+            "6.0.4"},
+            NON_REQUIRED_MESSAGES_UPDATES = {"6.0.0", "6.0.1", "6.0.2", "6.0.3", "6.0.4"};
 
     @Getter
     private boolean useProtocolLib;
