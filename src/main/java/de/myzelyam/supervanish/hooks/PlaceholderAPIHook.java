0da0a5709e26856c1abc0ea7a8ace5e56bd03e77
Leon Mangler <dev@myzelyam.de>
2020-07-28 00:17:23 +0200

    Update PlaceholderAPI hook to use the new expansion system

--- src/main/java/de/myzelyam/supervanish/hooks/PlaceholderAPIHook.java
+++ src/main/java/de/myzelyam/supervanish/hooks/PlaceholderAPIHook.java
@@ -9,78 +9,99 @@
 package de.myzelyam.supervanish.hooks;
 
 import de.myzelyam.supervanish.SuperVanish;
-
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 
 import java.util.Collection;
 import java.util.UUID;
-
-import me.clip.placeholderapi.PlaceholderAPI;
-import me.clip.placeholderapi.PlaceholderHook;
 
 public class PlaceholderAPIHook extends PluginHook {
 
     public PlaceholderAPIHook(SuperVanish superVanish) {
         super(superVanish);
+        PlaceholderAPI.registerExpansion(new SVPlaceholderExpansion());
+
     }
 
     public static String translatePlaceholders(String msg, Player p) {
         return PlaceholderAPI.setPlaceholders(p, msg);
     }
 
-    @Override
-    public void onPluginEnable(Plugin plugin) {
-        PlaceholderAPI.unregisterPlaceholderHook(superVanish);
-        PlaceholderAPI.registerPlaceholderHook(superVanish, new PlaceholderHook() {
-            @Override
-            public String onPlaceholderRequest(Player p, String id) {
-                try {
-                    if (id.equalsIgnoreCase("isvanished")
-                            || id.equalsIgnoreCase("isinvisible")
-                            || id.equalsIgnoreCase("vanished")
-                            || id.equalsIgnoreCase("invisible"))
-                        return superVanish.getVanishStateMgr().isVanished(p.getUniqueId()) ? "Yes"
-                                : "No";
-                    if (id.equalsIgnoreCase("onlinevanishedplayers")
-                            || id.equalsIgnoreCase("onlinevanished")
-                            || id.equalsIgnoreCase("invisibleplayers")
-                            || id.equalsIgnoreCase("vanishedplayers")
-                            || id.equalsIgnoreCase("hiddenplayers")) {
-                        Collection<UUID> onlineVanishedPlayers = superVanish.getVanishStateMgr()
-                                .getOnlineVanishedPlayers();
-                        String playerListMessage = "";
-                        for (UUID uuid : onlineVanishedPlayers) {
-                            Player onlineVanished = Bukkit.getPlayer(uuid);
-                            if (onlineVanished == null) continue;
-                            if (superVanish.getSettings().getBoolean(
-                                    "IndicationFeatures.LayeredPermissions.HideInvisibleInCommands", false)
-                                    && !superVanish.hasPermissionToSee(p, onlineVanished)) {
-                                continue;
-                            }
-                            playerListMessage = playerListMessage + onlineVanished.getName() + ", ";
+    public class SVPlaceholderExpansion extends PlaceholderExpansion {
+
+        @Override
+        public boolean persist() {
+            return true;
+        }
+
+        @Override
+        public boolean canRegister() {
+            return true;
+        }
+
+        @Override
+        public String getAuthor() {
+            return superVanish.getDescription().getAuthors().toString();
+        }
+
+        @Override
+        public String getIdentifier() {
+            return "supervanish";
+        }
+
+        @Override
+        public String getVersion() {
+            return superVanish.getDescription().getVersion();
+        }
+
+        @Override
+        public String onPlaceholderRequest(Player p, String id) {
+            try {
+                if (id.equalsIgnoreCase("isvanished")
+                        || id.equalsIgnoreCase("isinvisible")
+                        || id.equalsIgnoreCase("vanished")
+                        || id.equalsIgnoreCase("invisible"))
+                    return superVanish.getVanishStateMgr().isVanished(p.getUniqueId()) ? "Yes"
+                            : "No";
+                if (id.equalsIgnoreCase("onlinevanishedplayers")
+                        || id.equalsIgnoreCase("onlinevanished")
+                        || id.equalsIgnoreCase("invisibleplayers")
+                        || id.equalsIgnoreCase("vanishedplayers")
+                        || id.equalsIgnoreCase("hiddenplayers")) {
+                    Collection<UUID> onlineVanishedPlayers = superVanish.getVanishStateMgr()
+                            .getOnlineVanishedPlayers();
+                    String playerListMessage = "";
+                    for (UUID uuid : onlineVanishedPlayers) {
+                        Player onlineVanished = Bukkit.getPlayer(uuid);
+                        if (onlineVanished == null) continue;
+                        if (superVanish.getSettings().getBoolean(
+                                "IndicationFeatures.LayeredPermissions.HideInvisibleInCommands", false)
+                                && !superVanish.hasPermissionToSee(p, onlineVanished)) {
+                            continue;
                         }
-                        return playerListMessage.length() > 3
-                                ? playerListMessage.substring(0, playerListMessage.length() - 2)
-                                : playerListMessage;
+                        playerListMessage = playerListMessage + onlineVanished.getName() + ", ";
                     }
-                    if (id.equalsIgnoreCase("playercount")
-                            || id.equalsIgnoreCase("onlineplayers")) {
-                        int playercount = Bukkit.getOnlinePlayers().size();
-                        for (UUID uuid : superVanish.getVanishStateMgr()
-                                .getOnlineVanishedPlayers()) {
-                            Player onlineVanished = Bukkit.getPlayer(uuid);
-                            if (onlineVanished == null) continue;
-                            if (p == null || !superVanish.canSee(p, onlineVanished)) playercount--;
-                        }
-                        return playercount + "";
-                    }
-                } catch (Exception e) {
-                    superVanish.logException(e);
+                    return playerListMessage.length() > 3
+                            ? playerListMessage.substring(0, playerListMessage.length() - 2)
+                            : playerListMessage;
                 }
-                return "";
+                if (id.equalsIgnoreCase("playercount")
+                        || id.equalsIgnoreCase("onlineplayers")) {
+                    int playercount = Bukkit.getOnlinePlayers().size();
+                    for (UUID uuid : superVanish.getVanishStateMgr()
+                            .getOnlineVanishedPlayers()) {
+                        Player onlineVanished = Bukkit.getPlayer(uuid);
+                        if (onlineVanished == null) continue;
+                        if (p == null || !superVanish.canSee(p, onlineVanished)) playercount--;
+                    }
+                    return playercount + "";
+                }
+            } catch (Exception e) {
+                superVanish.logException(e);
             }
-        });
+            return null;
+        }
     }
 }
