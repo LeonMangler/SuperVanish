99b3a958bf63b4b3cded4bf050bf4c9585752e4d
Leon Mangler <dev@myzelyam.de>
2021-04-27 01:59:13 +0200

    Readd copyright headers to updated files

--- src/main/java/de/myzelyam/supervanish/features/SilentOpenChest.java
+++ src/main/java/de/myzelyam/supervanish/features/SilentOpenChest.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright © 2015, Leon Mangler and the SuperVanish contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package de.myzelyam.supervanish.features;
 
 import com.comphenix.protocol.ProtocolLibrary;
