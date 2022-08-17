b41a82d5339699c8541cea69b1784376e7a6546c
ARR4NN <70060472+ARR4NN@users.noreply.github.com>
2022-08-17 10:31:26 +0100

    Simplify

--- src/main/java/de/myzelyam/supervanish/features/NoDripLeafTilt.java
+++ src/main/java/de/myzelyam/supervanish/features/NoDripLeafTilt.java
@@ -1,6 +1,7 @@
 package de.myzelyam.supervanish.features;
 
 import de.myzelyam.supervanish.SuperVanish;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -12,17 +13,15 @@ public class NoDripLeafTilt extends Feature{
 
   @EventHandler(priority = EventPriority.HIGH)
   public void onInteract(PlayerInteractEvent e) {
-    if(plugin.getSettings().getBoolean("InvisibilityFeatures.DisableDripLeaf")) {
-      String material = e.getClickedBlock().getType().toString();
-      if (material.equals("BIG_DRIPLEAF")) {
-        e.setCancelled(true);
-      }
+    Player p = e.getPlayer();
+    if (!plugin.getVanishStateMgr().isVanished(p.getUniqueId())) return;
+    if (!e.getClickedBlock().getType().toString().equals("BIG_DRIPLEAF")) return;
+    e.setCancelled(true);
     }
-  }
 
 
   @Override
   public boolean isActive() {
-    return plugin.getSettings().getBoolean("InvisibilityFeatures.DisableDripLeaf");
+    return plugin.getSettings().getBoolean("InvisibilityFeatures.DisableDripLeaf",true);
   }
 }
