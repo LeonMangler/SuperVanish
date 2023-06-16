a5a83c6979798e70409754e4e8cd9daa4cf1093c
crazycat256 <63475640+crazycat256@users.noreply.github.com>
2023-06-16 12:03:13 +0200

    Fix a bug that leaked vanished players when a player logged in

--- src/main/java/de/myzelyam/supervanish/SuperVanish.java
+++ src/main/java/de/myzelyam/supervanish/SuperVanish.java
@@ -18,10 +18,7 @@ import de.myzelyam.supervanish.hooks.PluginHookMgr;
 import de.myzelyam.supervanish.net.UpdateNotifier;
 import de.myzelyam.supervanish.utils.ExceptionLogger;
 import de.myzelyam.supervanish.utils.VersionUtil;
-import de.myzelyam.supervanish.visibility.ActionBarMgr;
-import de.myzelyam.supervanish.visibility.FileVanishStateMgr;
-import de.myzelyam.supervanish.visibility.ServerListPacketListener;
-import de.myzelyam.supervanish.visibility.VisibilityChanger;
+import de.myzelyam.supervanish.visibility.*;
 import de.myzelyam.supervanish.visibility.hiders.PreventionHider;
 import lombok.Getter;
 import org.bukkit.Bukkit;
@@ -100,6 +97,8 @@ public class SuperVanish extends JavaPlugin implements SuperVanishPlugin {
                 actionBarMgr = new ActionBarMgr(this);
             if (useProtocolLib && ServerListPacketListener.isEnabled(this))
                 ServerListPacketListener.register(this);
+            if (useProtocolLib)
+                PlayerSpawnPacketListener.register(this);
             registerEvents();
             new PluginHookMgr(this);
             featureMgr = new FeatureMgr(this);
@@ -174,6 +173,9 @@ public class SuperVanish extends JavaPlugin implements SuperVanishPlugin {
         pluginManager.registerEvents(new GeneralListener(this), this);
         pluginManager.registerEvents(new PlayerBlockModifyListener(this), this);
         pluginManager.registerEvents(new WorldChangeListener(this), this);
+        pluginManager.registerEvents(new AdvancementListener(this), this);
+        pluginManager.registerEvents(new PickupListener(this), this);
+        pluginManager.registerEvents(new CommandListener(this), this);
         if (versionUtil.isOneDotXOrHigher(10)) {
             pluginManager.registerEvents(new TabCompleteListener(this), this);
         }
