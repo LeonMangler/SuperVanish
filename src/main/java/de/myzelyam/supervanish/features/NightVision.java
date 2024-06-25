bcf10d0688341679a806913ab4a863ff289ce61c
Leon Mangler <code@ml-m.dev>
2024-06-26 00:10:26 +0900

    6.2.19: - Redo night vision module without packets - Fix version util on latest paper

--- src/main/java/de/myzelyam/supervanish/features/NightVision.java
+++ src/main/java/de/myzelyam/supervanish/features/NightVision.java
@@ -8,8 +8,6 @@
 
 package de.myzelyam.supervanish.features;
 
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.PacketContainer;
 import de.myzelyam.api.vanish.PlayerHideEvent;
 import de.myzelyam.api.vanish.PlayerShowEvent;
 import de.myzelyam.supervanish.SuperVanish;
@@ -19,20 +17,20 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
-
-import static com.comphenix.protocol.PacketType.Play.Server.ENTITY_EFFECT;
-import static com.comphenix.protocol.PacketType.Play.Server.REMOVE_ENTITY_EFFECT;
 
 public class NightVision extends Feature implements Runnable {
 
-    private boolean suppressErrors = false;
-
     public static final int INFINITE_POTION_EFFECT_LENGTH = 32767;
+
+    private final Map<UUID, PotionEffect> playerPreviousPotionEffectMap = new HashMap<>();
 
     public NightVision(SuperVanish plugin) {
         super(plugin);
@@ -51,23 +49,52 @@ public class NightVision extends Feature implements Runnable {
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onVanish(PlayerHideEvent e) {
         Player p = e.getPlayer();
-        sendAddPotionEffect(p, new PotionEffect(PotionEffectType.NIGHT_VISION,
-                INFINITE_POTION_EFFECT_LENGTH, 0));
+        // getPotionEffect(..) is 1.10+ API
+        if (plugin.getVersionUtil().isOneDotXOrHigher(10)) {
+            if (p.getPotionEffect(PotionEffectType.NIGHT_VISION) != null) {
+                playerPreviousPotionEffectMap.put(p.getUniqueId(), p.getPotionEffect(PotionEffectType.NIGHT_VISION));
+            }
+        }
+        sendAddPotionEffect(p);
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onReappear(PlayerShowEvent e) {
         Player p = e.getPlayer();
-        sendRemovePotionEffect(p, PotionEffectType.NIGHT_VISION);
+        sendRemovePotionEffect(p);
+        if (playerPreviousPotionEffectMap.containsKey(p.getUniqueId())) {
+            p.addPotionEffect(playerPreviousPotionEffectMap.remove(p.getUniqueId()));
+        }
     }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
+        if (!plugin.getVanishStateMgr().isVanished(p.getUniqueId())) {
+            if (playerPreviousPotionEffectMap.containsKey(p.getUniqueId())) {
+                p.addPotionEffect(playerPreviousPotionEffectMap.remove(p.getUniqueId()));
+            }
+        } else {
+            sendAddPotionEffect(p);
+        }
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        Player p = e.getPlayer();
         if (!plugin.getVanishStateMgr().isVanished(p.getUniqueId()))
             return;
-        sendAddPotionEffect(p, new PotionEffect(PotionEffectType.NIGHT_VISION,
-                INFINITE_POTION_EFFECT_LENGTH, 0));
+        sendRemovePotionEffect(p);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onTeleport(PlayerTeleportEvent e) {
+        Player p = e.getPlayer();
+        if (!plugin.getVanishStateMgr().isVanished(p.getUniqueId()))
+            return;
+        if (e.getFrom().getWorld() == null || e.getTo() == null || e.getTo().getWorld() == null
+                || e.getFrom().getWorld().getName().equals(e.getTo().getWorld().getName())) return;
+        sendRemovePotionEffect(p);
     }
 
     @EventHandler
@@ -75,8 +102,7 @@ public class NightVision extends Feature implements Runnable {
         Player p = e.getPlayer();
         if (!plugin.getVanishStateMgr().isVanished(p.getUniqueId()))
             return;
-        sendAddPotionEffect(p, new PotionEffect(PotionEffectType.NIGHT_VISION,
-                INFINITE_POTION_EFFECT_LENGTH, 0));
+        sendAddPotionEffect(p);
     }
 
     @Override
@@ -85,91 +111,17 @@ public class NightVision extends Feature implements Runnable {
         for (UUID uuid : plugin.getVanishStateMgr().getOnlineVanishedPlayers()) {
             Player p = Bukkit.getPlayer(uuid);
             if (p == null) continue;
-            sendRemovePotionEffect(p, PotionEffectType.NIGHT_VISION);
-            sendAddPotionEffect(p, new PotionEffect(PotionEffectType.NIGHT_VISION,
-                    INFINITE_POTION_EFFECT_LENGTH, 0));
+            sendRemovePotionEffect(p);
+            sendAddPotionEffect(p);
         }
     }
 
-    private void sendAddPotionEffect(Player p, PotionEffect effect) {
-        try {
-            PacketContainer packet = new PacketContainer(ENTITY_EFFECT);
-            //noinspection deprecation
-            int effectID = effect.getType().getId();
-            int amplifier = effect.getAmplifier();
-            int duration = effect.getDuration();
-            int entityID = p.getEntityId();
-            if (plugin.getVersionUtil().isOneDotXOrHigher(19)) {
-                packet.getIntegers().write(0, entityID);
-                packet.getEffectTypes().write(0, effect.getType());
-                packet.getBytes().write(0, (byte) amplifier);
-                packet.getIntegers().write(1, duration);
-                // hide particles in 1.9
-                packet.getBytes().write(1, (byte) 0);
-            } else if (plugin.getVersionUtil().isOneDotXOrHigher(18) && packet.getIntegers().size() >= 3) {
-                // 1.18.2 changed effectID from byte to integer
-                packet.getIntegers().write(0, entityID);
-                packet.getIntegers().write(1, effectID);
-                packet.getBytes().write(0, (byte) amplifier);
-                packet.getIntegers().write(2, duration);
-                // hide particles in 1.9
-                packet.getBytes().write(1, (byte) 0);
-            } else {
-                packet.getIntegers().write(0, entityID);
-                packet.getBytes().write(0, (byte) effectID);
-                packet.getBytes().write(1, (byte) amplifier);
-                packet.getIntegers().write(1, duration);
-                // hide particles in 1.9
-                packet.getBytes().write(2, (byte) 0);
-            }
-            ProtocolLibrary.getProtocolManager().sendServerPacket(p, packet);
-        } catch (Exception | NoClassDefFoundError e) {
-            if (!suppressErrors) {
-                plugin.logException(e);
-                plugin.getLogger().warning("IMPORTANT: Please make sure that you are using the latest " +
-                        "dev-build of ProtocolLib and that your server is up-to-date! This error likely " +
-                        "happened inside of ProtocolLib code which is out of SuperVanish's control. It's part " +
-                        "of an optional feature module and can be removed safely by disabling " +
-                        "NightVisionEffect in the config file. Please report this " +
-                        "error if you can reproduce it on an up-to-date server with only latest " +
-                        "ProtocolLib and latest SV installed.");
-                suppressErrors = true;
-            }
-        }
+    private void sendAddPotionEffect(Player p) {
+        p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION,
+                INFINITE_POTION_EFFECT_LENGTH, 0, true, false));
     }
 
-    private void sendRemovePotionEffect(Player p, PotionEffectType type) {
-        try {
-            PacketContainer packet = new PacketContainer(REMOVE_ENTITY_EFFECT);
-            //noinspection deprecation
-            final int effectID = type.getId();
-            final int entityID = p.getEntityId();
-            // 1.7 and below
-            if (!plugin.getVersionUtil().isOneDotXOrHigher(8)) {
-                packet.getIntegers().write(0, entityID);
-                packet.getBytes().write(0, (byte) effectID);
-            } else if (plugin.getVersionUtil().isOneDotX(8)) {
-                // 1.8
-                packet.getIntegers().write(0, entityID);
-                packet.getIntegers().write(1, effectID);
-            } else {
-                // 1.9 and higher
-                packet.getEffectTypes().write(0, type);
-                packet.getIntegers().write(0, entityID);
-            }
-            ProtocolLibrary.getProtocolManager().sendServerPacket(p, packet);
-        } catch (Exception | NoClassDefFoundError e) {
-            if (!suppressErrors) {
-                plugin.logException(e);
-                plugin.getLogger().warning("IMPORTANT: Please make sure that you are using the latest " +
-                        "dev-build of ProtocolLib and that your server is up-to-date! This error likely " +
-                        "happened inside of ProtocolLib code which is out of SuperVanish's control. It's part " +
-                        "of an optional feature module and can be removed safely by disabling " +
-                        "NightVisionEffect in the config file. Please report this " +
-                        "error if you can reproduce it on an up-to-date server with only latest " +
-                        "ProtocolLib and latest SV installed.");
-                suppressErrors = true;
-            }
-        }
+    private void sendRemovePotionEffect(Player p) {
+        p.removePotionEffect(PotionEffectType.NIGHT_VISION);
     }
 }
